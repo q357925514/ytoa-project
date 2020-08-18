9dc6a014cfc91227ef4209e8ea25ece6b0631f9a
dengshuaiqiang <dsq627269813@163qq.com>
2020-08-18 22:30:38 +0800

    dsq1
    11

---
+++ ytoa-project/ytoa-eureka-information-module/src/test/java/com/ytoa/YtoaEurekaInformationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtoaEurekaInformationModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
