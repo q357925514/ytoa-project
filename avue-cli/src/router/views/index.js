6c5c11abbb8df5f31ec879626e6bb6ad8a98b64e
ly <ly3503083099@163.com>
2020-08-18 20:16:10 +0800

    VUE
    VUE

---
+++ avue-cli/src/router/views/index.js
@@ -0,0 +1,57 @@
+import Layout from '@/page/index/'
+export default [{
+  path: '/wel',
+  component: Layout,
+  redirect: '/wel/index',
+  children: [{
+    path: 'index',
+    name: '首页',
+    meta: {
+      i18n: 'dashboard'
+    },
+    component: () =>
+      import( /* webpackChunkName: "views" */ '@/views/wel/index')
+  }, {
+    path: 'dashboard',
+    name: '控制台',
+    meta: {
+      i18n: 'dashboard',
+      menu: false,
+    },
+    component: () =>
+      import( /* webpackChunkName: "views" */ '@/views/wel/dashboard')
+  }]
+}, {
+  path: '/form-detail',
+  component: Layout,
+  children: [{
+    path: 'index',
+    name: '详情页',
+    meta: {
+      i18n: 'detail'
+    },
+    component: () =>
+      import( /* webpackChunkName: "views" */ '@/views/util/form-detail')
+  }]
+}, {
+  path: '/info',
+  component: Layout,
+  redirect: '/info/index',
+  children: [{
+    path: 'index',
+    name: '个人信息',
+    meta: {
+      i18n: 'info'
+    },
+    component: () =>
+      import( /* webpackChunkName: "views" */ '@/views/user/info')
+  }, {
+    path: 'setting',
+    name: '个人设置',
+    meta: {
+      i18n: 'setting'
+    },
+    component: () =>
+      import( /* webpackChunkName: "views" */ '@/views/user/setting')
+  }]
+}]
